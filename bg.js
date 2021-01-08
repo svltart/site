aba2f0672f0183aab175501efe1099262822e1bf
Harry J <harryjiang0114@gmail.com>
2021-01-07 22:11:03 -0500

    sections

--- bg.js
+++ bg.js
@@ -1,17 +1,17 @@
-$(window).scroll(function() {
-  if ($('#blue').offset().top - $(window).scrollTop() < 300) {
+$(window).scroll($.debounce(50, function(){
+  if ($('#social').offset().top - $(window).scrollTop() < 100) {
     $("#moon").slideUp("slow");
   } else {
     $("#moon").slideDown();
   }
-  if ($('#green').offset().top - $(window).scrollTop() < 300) {
+  if ($('#art').offset().top - $(window).scrollTop() < 200) {
     $("#ultra-moon").slideUp("slow");
   } else {
     $("#ultra-moon").slideDown();
   }
-  if ($('#yellow').offset().top - $(window).scrollTop() < 300) {
+  if ($('#commissions').offset().top - $(window).scrollTop() < 500) {
     $("#ultra-sun").slideUp("slow");
   } else {
     $("#ultra-sun").slideDown();
   }
-});
+}));
