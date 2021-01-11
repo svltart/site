0a963070da324cc1392e4208b839564ba12c9c0a
Harry J <harryjiang0114@gmail.com>
2021-01-10 20:05:25 -0500

    home button

--- bg.js
+++ bg.js
@@ -1,7 +1,9 @@
 $(window).scroll($.debounce(50, function(){
   if ($('#shop').offset().top - $(window).scrollTop() < 250) {
+    $(".go-home").slideDown();
     $("#moon").slideUp("slow");
   } else {
+    $(".go-home").slideUp();
     $("#moon").slideDown();
   }
   if ($('#commissions').offset().top - $(window).scrollTop() < 200) {
@@ -9,7 +11,7 @@ $(window).scroll($.debounce(50, function(){
   } else {
     $("#ultra-moon").slideDown();
   }
-  if ($('#art').offset().top - $(window).scrollTop() < 300) {
+  if ($('#art').offset().top - $(window).scrollTop() < 100) {
     $("#sun").slideUp("slow");
   } else {
     $("#sun").slideDown();
